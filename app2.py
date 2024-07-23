29da49125eaa89d2e5f0a1eeefc2af8b4fbc4791
Shakil <sayyadshakilgdsc@gmail.com>
2024-07-23 18:57:29 +0530

    Updated app2.py

--- app2.py
+++ app2.py
@@ -5,11 +5,11 @@ import textwrap
 
 app = Flask(__name__)
 
-# Load the fine-tuned model and tokenizer
+
 tokenizer = AutoTokenizer.from_pretrained("./fine-tuned-model")
 model = AutoModelForSequenceClassification.from_pretrained("./fine-tuned-model")
 
-# Create a pipeline for sentiment analysis
+
 sentiment_analyzer = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 
 GOOGLE_API_KEY = "AIzaSyDB5i8h8QV1BVgG-ZO9JDh1EicU8luhPrs"
@@ -28,7 +28,7 @@ def predict_sentiment():
     if not message:
         return jsonify({"error": "No message provided"}), 400
 
-    # Perform sentiment analysis using the pipeline
+   
     result = sentiment_analyzer(message)
     sentiment = result[0]['label']
     response_message = ""
